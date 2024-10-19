8e558d6a3c0fc3a88d9102dc56d90d22451376ae
Pamel0 <93038245+Pamel0@users.noreply.github.com>
2024-10-19 21:12:22 +0200

    Update Zadanie3.cs

--- Zadanie3.cs
+++ Zadanie3.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
  
-public class SquareMovement : MonoBehaviour
+public class Zadanie3 : MonoBehaviour
 {
     public float speed = 2f; // Prędkość ruchu
     private Vector3[] corners;
@@ -8,7 +8,6 @@ public class SquareMovement : MonoBehaviour
  
     void Start()
     {
-        // Definiowanie wierzchołków kwadratu
         corners = new Vector3[4];
         corners[0] = new Vector3(5, 0, 5);
         corners[1] = new Vector3(-5, 0, 5);
@@ -23,13 +22,10 @@ public class SquareMovement : MonoBehaviour
  
     void MoveTowardsCorner()
     {
-        // Ruch w kierunku bieżącego wierzchołka
         transform.position = Vector3.MoveTowards(transform.position, corners[currentCorner], speed * Time.deltaTime);
  
-        // Sprawdzanie, czy obiekt dotarł do wierzchołka
         if (Vector3.Distance(transform.position, corners[currentCorner]) < 0.1f)
         {
-            // Obrót o 90 stopni
             transform.Rotate(0, 90, 0);
             currentCorner = (currentCorner + 1) % corners.Length; // Przechodzenie do następnego wierzchołka
         }
