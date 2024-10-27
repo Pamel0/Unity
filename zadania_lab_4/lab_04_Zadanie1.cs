585eb9a6955f5b8263260599d4d9fdee10ef3fa6
Pamel0 <93038245+Pamel0@users.noreply.github.com>
2024-10-27 21:49:32 +0100

    Update lab_04_Zadanie1.cs

--- zadania_lab_4/lab_04_Zadanie1.cs
+++ zadania_lab_4/lab_04_Zadanie1.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RandomCubesGenerator : MonoBehaviour
+public class Zadanie1 : MonoBehaviour
 {
     public GameObject block;
     public Material[] materials;
